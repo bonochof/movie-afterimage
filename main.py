aa839c33a266489864fe9704b81091899f5428a7
bonochof <ryos427@yahoo.co.jp>
2020-06-21 20:52:07 +0900

    メディアンフィルタによるノイズ除去

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ while True:
         break
 
     fgmask = fgbg.apply(frame)
+    fgmask = cv2.medianBlur(fgmask, 5)
     img_src = frame
     if img_sum is None:
         img_sum = img_src
@@ -23,10 +24,10 @@ while True:
         img_src_and = cv2.bitwise_and(frame, frame, mask=fgmask)
         img_src_rgba = cv2.cvtColor(img_src_and, cv2.COLOR_RGB2RGBA)
         img_src_rgba[..., 3] = np.where(np.all(img_src_and==0, axis=-1), 0, 255)
-        #img_src_rgba[fgmask==0] = fgmask
-        img_sum = cv2.bitwise_or(img_sum_rgba, img_src_rgba)#cv2.addWeighted(img_sum_rgba, 0.5, img_src_rgba, 0.5, 0)
-        #height, width = img_sum.shape[:2]
-        #img_sum_rgba[0:height, 0:width] += img_src_rgba
+        #img_sum = img_sum_rgba + img_src_rgba
+        img_sum = cv2.bitwise_or(img_sum_rgba, img_src_rgba)
+        #img_sum = img_sum_rgba + img_or
+        #img_sum = cv2.add(img_sum_rgba, img_src_rgba)
 
     cv2.imshow("Frame", img_sum)
     key = cv2.waitKey(30)
