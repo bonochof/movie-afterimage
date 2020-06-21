d6646af82f8a2e546a48ac0c192349b1131543af
bonochof <ryos427@yahoo.co.jp>
2020-06-21 21:04:23 +0900

    合成方法を変更

--- main.py
+++ main.py
@@ -19,15 +19,13 @@ while True:
     if img_sum is None:
         img_sum = img_src
     else:
-        #img_src_rgba = cv2.cvtColor(img_src, cv2.COLOR_RGB2RGBA)
         img_sum_rgba = cv2.cvtColor(img_sum, cv2.COLOR_RGB2RGBA)
         img_src_and = cv2.bitwise_and(frame, frame, mask=fgmask)
         img_src_rgba = cv2.cvtColor(img_src_and, cv2.COLOR_RGB2RGBA)
         img_src_rgba[..., 3] = np.where(np.all(img_src_and==0, axis=-1), 0, 255)
-        #img_sum = img_sum_rgba + img_src_rgba
-        img_sum = cv2.bitwise_or(img_sum_rgba, img_src_rgba)
-        #img_sum = img_sum_rgba + img_or
-        #img_sum = cv2.add(img_sum_rgba, img_src_rgba)
+        img_sum_add = img_sum_rgba + img_src_rgba
+        #img_sum = cv2.bitwise_or(img_sum_rgba, img_src_rgba)
+        img_sum = cv2.addWeighted(img_sum_add, 1.0, img_src_rgba, 1.0, 0)
 
     cv2.imshow("Frame", img_sum)
     key = cv2.waitKey(30)
